9c3e098a57e1909ba386e3e3175616de23d546ec
Victor Kauan <victorkauan.dev@gmail.com>
2022-11-15 22:03:17 -0300

    feat and refactor: create and style navigation bar partial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{handlebars,js}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      black: colors.black,
+      white: colors.white,
+      'light-green': '#4AB087',
+      'dark-green': '#3F8963',
+      'primary-yellow': '#FED031',
+      'primary-red': '#AC2626',
+    },
   },
   plugins: [],
 };
