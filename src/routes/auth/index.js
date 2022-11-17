1839be2a056e405b39bac6269eb2fb091ad298c6
Victor Kauan <victorkauan.dev@gmail.com>
2022-11-17 11:29:25 -0300

    feat: encrypt user passwords with BcryptJS

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,6 +1,8 @@
 // - Express
 const express = require('express');
 const router = express.Router();
+// - BcryptJS
+const bcrypt = require('bcryptjs');
 // - Data functions
 const { getUsers, updateUsers } = require('../../utils/data');
 
@@ -21,10 +23,13 @@ router.post('/cadastrar', async (req, res) => {
   const users = await getUsers();
   const newUsers = [...users];
 
+  // Encrypt password
+  const hash = await bcrypt.hash(password, 10);
+
   newUsers.push({
     id: String(Number(newUsers[newUsers.length - 1].id) + 1),
     email,
-    password,
+    password: hash,
     first_name: firstName,
     last_name: lastName,
     is_admin: String(false),
