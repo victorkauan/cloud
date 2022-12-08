bc3e36813b6811da594e289441df4f5e05b987b4
Victor Kauan <victorkauan.dev@gmail.com>
2022-12-08 16:35:53 -0300

    fix: Update mock files paths

--- src/utils/mockPaths.js
+++ src/utils/mockPaths.js
@@ -1,13 +1,11 @@
 // - Path
 const path = require('path');
 
-const basicPath = path.resolve(__dirname, '..', '..', 'tmp');
-
 module.exports = {
   mockPaths: {
-    products: path.join(basicPath, 'products.json'),
-    categories: path.join(basicPath, 'categories.json'),
-    users: path.join(basicPath, 'users.json'),
-    carts: path.join(basicPath, 'carts.json'),
+    products: '/tmp/products.json',
+    categories: '/tmp/categories.json',
+    users: '/tmp/users.json',
+    carts: '/tmp/carts.json',
   },
 };
